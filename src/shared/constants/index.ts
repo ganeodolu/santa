4d183bc9ec0ac21b44fcf7eec5cf7cbc8b51f4e3
ganeodolu <ganeodolu@gmail.com>
2024-07-25 16:56:15 +0900

    fix: 공원정보 키 영문으로 변경

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,12 +1,12 @@
 export const parkNames = ["북한산", "설악산"];
 
 export const parkInformation = {
-  북한산: {
+  "bukhan": {
     name: "북한산",
     lat: 37.645175709619025,
     lon: 126.97477909129488
   },
-  설악산: {
+  "seorak": {
     name: "설악산",
     lat: 38.17151087237107,
     lon: 128.43856899800753
