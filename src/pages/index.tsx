4b1c259b4c732b2bec5b9316972e59948c8b4047
ganeodolu <ganeodolu@gmail.com>
2024-07-25 16:55:33 +0900

    feat: 기본 쿼리스트링 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import Home from "@/views/Home";
-
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 
 export default function IndexPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const search = searchParams.get("parkName");
+
+  useEffect(() => {
+    if (!search) {
+      router.push({
+        query: { parkName: "bukhan" }
+      });
+    }
+  }, [])
+  
   return (
     <main
       className={`min-h-screen flex-col items-center justify-between p-24`}
