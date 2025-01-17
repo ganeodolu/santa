fc5c61507aeba2c9b01572425a367816a7348d46
ganeodolu <ganeodolu@gmail.com>
2025-01-17 18:06:19 +0900

    feat: 검색 페이지 지정

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,12 @@
+import SearchView from '@/views/searchView'
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className={`bg-gray-100`}>
+      <SearchView />
+    </div>
+  );
+}
+
+export default Search
