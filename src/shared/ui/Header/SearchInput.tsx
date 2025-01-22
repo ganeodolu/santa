8e4688e22ae9f88a1765312a14751a2581236b38
ganeodolu <ganeodolu@gmail.com>
2025-01-22 17:57:41 +0900

    refactor: 헤더에서 검색과 뒤로가기 분리

---
+++ src/shared/ui/Header/SearchInput.tsx
@@ -0,0 +1,36 @@
+type SearchInputProps = {
+  value: string;
+  onChange: (value: string) => void;
+  onClick?: () => void;
+};
+
+const SearchInput = ({ value, onChange, onClick }: SearchInputProps) => (
+  <search className="relative flex-grow">
+    <input
+      type="search"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      className="w-full rounded-lg border border-gray-300 py-2 pl-10 pr-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      placeholder="산 찾기"
+      onClick={onClick}
+    />
+    <div className="absolute left-3 top-2">
+      <svg
+        className="h-6 w-6 text-gray-400"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+    </div>
+  </search>
+);
+
+export default SearchInput;
