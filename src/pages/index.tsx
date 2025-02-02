9d29672f0e5f2806575e6c4634f88cf35475454e
ganeodolu <ganeodolu@gmail.com>
2025-02-02 20:21:12 +0900

    style: tailwindcss 형식 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import HomeView from "@/views/homeView";
 
 export default function IndexPage() {
   return (
-    <div className={`min-h-screen bg-gray-100`}>
+    <div className="min-h-screen bg-gray-100">
       <HomeView />
     </div>
   );
