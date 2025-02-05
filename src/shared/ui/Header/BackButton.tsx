5ded38925af1032d7c604f8f42145be5583122cf
ganeodolu <ganeodolu@gmail.com>
2025-02-05 21:57:35 +0900

    feat: 뒤로가기 버튼 기능 추가

--- src/shared/ui/Header/BackButton.tsx
+++ src/shared/ui/Header/BackButton.tsx
@@ -1,20 +1,26 @@
-const BackButton = () => (
-  <button className="mr-3">
-    <svg
-      className="h-6 w-6 text-gray-600"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M10 19l-7-7m0 0l7-7m-7 7h18"
-      />
-    </svg>
-  </button>
-);
+import { useRouter } from "next/router";
+
+const BackButton = () => {
+  const router = useRouter();
+
+  return (
+    <button className="mr-3 cursor-pointer" onClick={() => router.back()}>
+      <svg
+        className="h-6 w-6 text-gray-600"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M10 19l-7-7m0 0l7-7m-7 7h18"
+        />
+      </svg>
+    </button>
+  );
+};
 
 export default BackButton;
