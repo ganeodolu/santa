ef06e38b6d4783c9310c22a2ee45dfcebf3a86e8
ganeodolu <ganeodolu@gmail.com>
2025-02-05 22:00:16 +0900

    style: cursor pointer 추가

--- src/shared/ui/Header/SearchInput.tsx
+++ src/shared/ui/Header/SearchInput.tsx
@@ -10,11 +10,11 @@ const SearchInput = ({ value, onChange, onClick }: SearchInputProps) => (
       type="search"
       value={value}
       onChange={(e) => onChange(e.target.value)}
-      className="w-full rounded-lg border border-gray-300 py-2 pl-10 pr-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      className="w-full cursor-pointer rounded-lg border border-gray-300 py-2 pr-4 pl-10 focus:ring-2 focus:ring-blue-500 focus:outline-none"
       placeholder="산 찾기"
       onClick={onClick}
     />
-    <div className="absolute left-3 top-2">
+    <div className="absolute top-2 left-3">
       <svg
         className="h-6 w-6 text-gray-400"
         fill="none"
