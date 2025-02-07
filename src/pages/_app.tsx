b48da0c214fc76791eeabae1ca95c4c5e65128fd
ganeodolu <ganeodolu@gmail.com>
2025-02-07 18:48:42 +0900

    build: vercel analytics 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import { Provider } from "jotai";
+import type { AppProps } from "next/app";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,11 +15,14 @@ const queryClient = new QueryClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Provider>
-        <Component {...pageProps} />
-      </Provider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Provider>
+          <Component {...pageProps} />
+        </Provider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+      <Analytics />
+    </>
   );
 }
