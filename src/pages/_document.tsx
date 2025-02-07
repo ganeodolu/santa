8349ae1c952eaada64dec32a35a399b7642af45b
ganeodolu <ganeodolu@gmail.com>
2025-02-07 20:58:50 +0900

    feat: meta 태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>
+          산타보아
+        </title>
+        <meta
+          name="description"
+          content="국립공원 산 정보를 산타보아에서 알아보세요"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
