5bd0c5900fe39e1d8661090a51dd6b0596c49eae
ganeodolu <ganeodolu@gmail.com>
2025-02-13 19:48:16 +0900

    feat: open graph 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Analytics } from "@vercel/analytics/react";
 import { Provider } from "jotai";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,6 +17,23 @@ const queryClient = new QueryClient({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>산타보아</title>
+        <meta
+          name="description"
+          content="국립공원 산 정보를 알려주는 산타보아입니다."
+        />
+        <meta property="og:title" content="산타보아" />
+        <meta property="og:site_name" content="Santaboa" />
+        <meta
+          property="og:description"
+          content="국립공원 산 정보를 알려주는 산타보아입니다."
+        />
+        <meta
+          property="og:image"
+          content="/images/ogImage.png"
+        />
+      </Head>
       <QueryClientProvider client={queryClient}>
         <Provider>
           <Component {...pageProps} />
