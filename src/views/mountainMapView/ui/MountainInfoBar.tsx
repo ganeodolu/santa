52862702ecfd68738ac22186edc4f16f0f13e366
ganeodolu <ganeodolu@gmail.com>
2025-02-21 19:43:58 +0900

    feat: 하단 Bar 클릭시 산 페이지 이동 추가

--- src/views/mountainMapView/ui/MountainInfoBar.tsx
+++ src/views/mountainMapView/ui/MountainInfoBar.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import type { Mountain } from "@/shared/constants";
+import { useNavigation } from "@/shared/model/useNavigation";
 
 type Props = {
   mountainInfo: Mountain;
 };
 
-const MountainInfoBar = ({ mountainInfo: { name, imageSrc, region, height } }: Props) => {
+const MountainInfoBar = ({ mountainInfo: { name, englishName, imageSrc, region, height } }: Props) => {
+  const { navigateTo } = useNavigation();
+  
   return (
-    <section className="fixed right-0 bottom-0 left-0 z-[1000] mx-auto flex h-30 max-w-[500px] items-center bg-white p-4 shadow-lg">
+    <section 
+      className="fixed right-0 bottom-0 left-0 z-[1000] mx-auto flex h-30 max-w-[500px] items-center bg-white p-4 shadow-lg"
+      onClick={() => navigateTo(`/mountain/${englishName}`)}
+    >
       <div className="relative mr-4 h-[100px] w-[200px] overflow-hidden">
         <Image
           src={imageSrc}
