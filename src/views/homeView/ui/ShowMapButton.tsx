9a13e7af3fc5fd586170fae057a6b9bd5328eefd
ganeodolu <ganeodolu@gmail.com>
2025-02-26 20:09:00 +0900

    style: 지도보기 hover시 색 변경

--- src/views/homeView/ui/ShowMapButton.tsx
+++ src/views/homeView/ui/ShowMapButton.tsx
@@ -8,7 +8,7 @@ const ShowMapButton = ({ href }: Props) => {
   return (
     <div className="fixed bottom-4 left-1/2 -translate-x-1/2 transform">
       <Link
-        className="inline-block w-[100px] rounded-lg bg-cyan-800 py-2 text-center font-bold text-white"
+        className="inline-block w-[100px] rounded-lg bg-cyan-800 py-2 text-center font-bold text-white hover:bg-cyan-600"
         href={href}
       >
         지도보기
