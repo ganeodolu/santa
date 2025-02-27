11fb2b4e399a160d73917d9df76f7dacc3260f5e
ganeodolu <ganeodolu@gmail.com>
2025-02-27 20:31:39 +0900

    feat: 산 설명 추가 및 스타일 수정

--- src/views/mountainView/index.tsx
+++ src/views/mountainView/index.tsx
@@ -44,7 +44,7 @@ type Props = {
 
 const MountainView = ({ mountainData }: Props) => {
   const [isMounted, setIsMounted] = useState(false);
-  const { name, lat, lon, height, peak, region, imageSrc, englishName, cctv } =
+  const { name, lat, lon, height, peak, region, imageSrc, englishName, cctv, introduction } =
     mountainData;
   const { x, y } = xyConvert(lat, lon);
   const { data: weatherData } = useQuery({
@@ -81,104 +81,108 @@ const MountainView = ({ mountainData }: Props) => {
         )}
       </section>
 
-      <section className="mb-4 flex">
-        <div className="w-1/2 pr-4 pl-2">
+      <section className="mb-2 flex">
+        <div className="h-[140px] w-[220px] overflow-hidden pr-4 pl-2">
           <Image
-            className="rounded-lg shadow-lg"
+            className="rounded-lg"
             src={imageSrc}
             alt={name}
-            width={234}
-            height={160}
+            width={200}
+            height={140}
           />
         </div>
         <div className="w-1/2 pl-4">
-          <h1 className="mb-4 text-3xl font-bold">{name}</h1>
-          <p className="mb-2">
+          <h1 className="mb-2 text-2xl font-bold">{name}</h1>
+          <p className="mb-1">
             높이:{" "}
             <span className="font-semibold">
               {height.toLocaleString("en-US")}m
             </span>
           </p>
-          <p className="mb-2">
+          <p className="mb-1">
             지역: <span className="font-semibold">{region}</span>
           </p>
-          <p className="mb-2">
+          <p className="mb-1">
             주요봉: <span className="font-semibold">{peak}</span>
           </p>
         </div>
       </section>
+      <section className="ml-2 mr-2">
+        <h3 className="text-justify text-sm">{introduction}</h3>
+      </section>
 
-      {weatherData && <section className="mb-4 rounded-lg bg-white">
-        <h2 className="mb-2 text-center text-2xl font-bold">날씨</h2>
-        <ResponsiveContainer width="100%" height={200}>
-          <ComposedChart data={weatherData}>
-            <CartesianGrid
-              horizontal={true}
-              vertical={true}
-              strokeDasharray="3 3"
-            />
-            <XAxis
-              dataKey="timestamp"
-              interval={0}
-              tickFormatter={formatXAxis}
-            />
-            <YAxis
-              type="number"
-              yAxisId="TMP"
-              domain={["dataMin - 10", "dataMax + 2"]}
-              ticks={generateYAxisTicks(
-                weatherData as weatherDataProps[],
-                "TMP",
-                -5,
-                2
-              )}
-              tick={{ fill: "#ee1b1b" }}
-              label={{ value: "°C", position: "insideTopLeft" }}
-              orientation="left"
-            />
-            <YAxis
-              yAxisId="POP"
-              domain={[0, 400]}
-              ticks={[0, 100]}
-              tick={{ fill: "#3236a8" }}
-              label={{ value: "%", position: "insideTopLeft" }}
-              orientation="right"
-            />
-            <Tooltip content={CustomTooltip} />
-            <Legend />
-            <Line
-              yAxisId="TMP"
-              type="monotone"
-              dataKey="TMP"
-              stroke="#ee1b1b"
-              name="기온 (°C)"
-              dot={CustomizedDot}
-              animationDuration={0}
-            />
-            <Bar
-              yAxisId="POP"
-              type="monotone"
-              dataKey="POP"
-              fill="#3236a8"
-              name="강수확률 (%)"
-            />
-            {/* <Bar
+      {weatherData && (
+        <section className="mb-4 rounded-lg bg-white">
+          <h2 className="mb-2 text-center text-2xl font-bold">날씨</h2>
+          <ResponsiveContainer width="100%" height={200}>
+            <ComposedChart data={weatherData}>
+              <CartesianGrid
+                horizontal={true}
+                vertical={true}
+                strokeDasharray="3 3"
+              />
+              <XAxis
+                dataKey="timestamp"
+                interval={0}
+                tickFormatter={formatXAxis}
+              />
+              <YAxis
+                type="number"
+                yAxisId="TMP"
+                domain={["dataMin - 10", "dataMax + 2"]}
+                ticks={generateYAxisTicks(
+                  weatherData as weatherDataProps[],
+                  "TMP",
+                  -5,
+                  2
+                )}
+                tick={{ fill: "#ee1b1b" }}
+                label={{ value: "°C", position: "insideTopLeft" }}
+                orientation="left"
+              />
+              <YAxis
+                yAxisId="POP"
+                domain={[0, 400]}
+                ticks={[0, 100]}
+                tick={{ fill: "#3236a8" }}
+                label={{ value: "%", position: "insideTopLeft" }}
+                orientation="right"
+              />
+              <Tooltip content={CustomTooltip} />
+              <Legend />
+              <Line
+                yAxisId="TMP"
+                type="monotone"
+                dataKey="TMP"
+                stroke="#ee1b1b"
+                name="기온 (°C)"
+                dot={CustomizedDot}
+                animationDuration={0}
+              />
+              <Bar
+                yAxisId="POP"
+                type="monotone"
+                dataKey="POP"
+                fill="#3236a8"
+                name="강수확률 (%)"
+              />
+              {/* <Bar
               yAxisId="right"
               dataKey="precipitation"
               fill="#ffc658"
               name="강수량 (mm)"
             /> */}
-            {/* <Line
+              {/* <Line
               yAxisId="left"
               type="monotone"
               dataKey="WSD"
               stroke="#ff7300"
               name="풍속 (m/s)"
             /> */}
-          </ComposedChart>
-        </ResponsiveContainer>
-      </section>
-      }
+            </ComposedChart>
+          </ResponsiveContainer>
+        </section>
+      )}
       <CCTVExternalLink cctv={cctv} />
     </article>
   );
