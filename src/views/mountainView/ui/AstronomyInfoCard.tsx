6fdd7ead17cfca06fe84eef05971319a25f629d7
ganeodolu <ganeodolu@gmail.com>
2025-03-03 00:30:16 +0900

    style: 일출 일몰 스타일 수정

--- src/views/mountainView/ui/AstronomyInfoCard.tsx
+++ src/views/mountainView/ui/AstronomyInfoCard.tsx
@@ -14,15 +14,19 @@ type Props = {
 const AstronomyInfoCard = ({astronomyData: {sunrise, sunset}}: Props) => {
   return (
     <section className="flex w-full">
-      <div className="flex flex-1 items-center justify-center border-r p-2">
-        <SunriseIcon width={50} height={50} />
-        <span className="text-lg font-semibold">
+      <div className="flex flex-1 items-center justify-center border-r">
+        <span className="text-base font-semibold">일출</span>
+        <SunriseIcon width={40} height={40} />
+        <span className="text-base font-semibold ml-2">
           {dayjs(sunrise, "HHmm").format("A hh:mm")}
         </span>
       </div>
-      <div className="flex flex-1 items-center justify-center p-2">
-        <SunsetIcon width={50} height={50} />
-        <span className="text-lg font-semibold">{dayjs(sunset, "HHmm").format("A hh:mm")}</span>
+      <div className="flex flex-1 items-center justify-center">
+        <span className="text-base font-semibold">일몰</span>
+        <SunsetIcon width={40} height={40} />
+        <span className="text-base font-semibold ml-2">
+          {dayjs(sunset, "HHmm").format("A hh:mm")}
+        </span>
       </div>
     </section>
   );
