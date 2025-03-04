16be5bccb6ef1c367657f26a71ccc768b5006f59
ganeodolu <ganeodolu@gmail.com>
2025-03-04 14:03:08 +0900

    style: Link로 인한 스타일 미적용 수정

--- src/views/homeView/ui/MountainList.tsx
+++ src/views/homeView/ui/MountainList.tsx
@@ -12,21 +12,20 @@ const MountainList = ({ mountainList }: Props) => {
       <div className="-mx-2 flex flex-wrap">
         {mountainList.map(({ name, englishName, region, imageSrc }) => (
           <div key={englishName} className="mb-4 w-1/2 px-2">
-            <Link
-              className="overflow-hidden rounded-lg bg-white shadow-md"
-              href={`/mountain/${englishName}`}
-            >
-              <Image
-                className="h-[140px] rounded-lg object-cover shadow-lg"
-                src={imageSrc}
-                alt={name}
-                width={234}
-                height={140}
-              />
-              <div className="flex items-center gap-2 p-4">
-                <h3 className="text-lg font-bold">{name}</h3>
-                <span className="text-gray-600">{region}</span>
-              </div>
+            <Link href={`/mountain/${englishName}`}>
+              <figure className="overflow-hidden rounded-lg bg-white shadow-md">
+                <Image
+                  className="h-[140px] rounded-lg object-cover"
+                  src={imageSrc}
+                  alt={name}
+                  width={234}
+                  height={140}
+                />
+                <div className="flex items-center gap-2 p-4">
+                  <h3 className="text-lg font-bold">{name}</h3>
+                  <span className="text-gray-600">{region}</span>
+                </div>
+              </figure>
             </Link>
           </div>
         ))}
