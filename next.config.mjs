ec9c9dd3d3b46b4ecdcbd190e225b39250a8c68e
ganeodolu <ganeodolu@gmail.com>
2025-03-04 22:40:39 +0900

    build: file-loader 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -2,24 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.module.rules.push(
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ["@svgr/webpack"]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name].[ext]",
-            publicPath: "/_next/static/fonts/",
-            outputPath: "static/fonts/"
-          }
-        }
-      }
-    );
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"]
+    });
     return config;
   },
   images: {
