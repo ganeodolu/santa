d2321ae50a3059f1230d2461ce8544ae0aa341d9
ganeodolu <ganeodolu@gmail.com>
2025-03-18 21:36:41 +0900

    fix: metatag 위치 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,6 @@ import { Analytics } from "@vercel/analytics/react";
 import { Provider } from "jotai";
 import type { AppProps } from "next/app";
 import localFont from "next/font/local";
-import Head from "next/head";
 import { useState } from "react";
 
 export const pretendard = localFont({
@@ -42,20 +41,6 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Head>
-        <title>산타보아</title>
-        <meta
-          name="description"
-          content="국립공원 산 정보를 알려주는 산타보아입니다."
-        />
-        <meta property="og:title" content="산타보아" />
-        <meta property="og:site_name" content="SANTABOA" />
-        <meta
-          property="og:description"
-          content="국립공원 산 정보를 알려주는 산타보아입니다."
-        />
-        <meta property="og:image" content="/ogImage.png" />
-      </Head>
       <QueryClientProvider client={queryClient}>
         <HydrationBoundary state={pageProps.dehydratedState}>
           <Provider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,23 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko" className={`${pretendard.variable}`}>
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>산타보아</title>
+        <meta name="description" content="산타보아" />
+        <meta
+          name="keywords"
+          content="산타보아, 국립공원, 산, 날씨, 지도, mountain"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="산타보아" />
+        <meta property="og:site_name" content="SANTABOA" />
+        <meta
+          property="og:description"
+          content="국립공원 산 정보를 알려주는 산타보아입니다."
+        />
+        <meta property="og:image" content="/ogImage.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
