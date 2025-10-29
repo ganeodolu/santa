e33af1d73dc150f7aa599f1c86e8713eb46dca92
ganeodolu <ganeodolu@gmail.com>
2025-10-29 19:17:47 +0900

    feat: 공통 메타 태그 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import type { AppProps } from "next/app";
 import localFont from "next/font/local";
 import Head from "next/head";
 import type { ReactElement, ReactNode } from "react";
+import MetaTags from "@/shared/ui/MetaTags";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -34,24 +35,14 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <>
+      <MetaTags
+        title="산타보아"
+        description="국립공원 산에 대한 모든 것! 18개 산악형 국립공원의 지도, 날씨, 일출, 일몰, CCTV 정보를 알려드립니다."
+        keywords="산타보아, 국립공원, 산, 날씨, 위치, 지도, mountain"
+      />
       <Head>
-        <title>산타보아</title>
-        <meta
-          name="description"
-          content="국립공원 산 정보를 알려주는 산타보아입니다."
-        />
-        <meta
-          name="keywords"
-          content="산타보아, 국립공원, 산, 날씨, 지도, mountain"
-        />
-        <meta name="robots" content="index, follow" />
-        <meta property="og:title" content="산타보아" />
         <meta property="og:site_name" content="SANTABOA" />
-        <meta
-          property="og:description"
-          content="국립공원 산 정보를 알려주는 산타보아입니다."
-        />
-        <meta property="og:image" content="/ogImage.png" />
+        <meta name="robots" content="index, follow" />
         <meta
           name="google-site-verification"
           content="llNbGRX6IDXM0PEjuLvk7eFRjAWorst3cG-qU0GOFT8"
